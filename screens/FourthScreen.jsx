e43f74b45fe93893bc68d90d984f9d637a23cb8f
Emmanuel Jemeni <jemenichinonso11@gmail.com>
2023-01-17 10:55:20 +0100

    Created our screens

---
+++ screens/FourthScreen.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const FourthScreen = () => {
+  return (
+    <View>
+      <Text>FourthScreen</Text>
+    </View>
+  )
+}
+
+export default FourthScreen
+
+const styles = StyleSheet.create({})
